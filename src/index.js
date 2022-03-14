caceddfe9ab19d8c83aa1253c4593015ef0ed771
arshdeep singh <arshdeep4909@gmail.com>
2022-03-14 19:30:15 -0400

    inner workings or middleWare.

--- src/index.js
+++ src/index.js
@@ -20,20 +20,18 @@ const unsubscribe = store.subscribe(() => {
 //based on the type of action it gets the reducer and then changes the state accordingly
 // const newState = reducer(state, action) how source code for dispatch looks like
 store.dispatch(userAdded({ name: "Arsh" }));
-store.dispatch(userAdded({ name: "Mosh" }));
-store.dispatch(projectAdded({ name: "project 1" }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(bugAdded({ description: "Bug 4" }));
-store.dispatch(bugAdded({ description: "Bug 5" }));
-store.dispatch(bugAdded({ description: "Bug 6" }));
-const bugByUserId = getBugsByUser(1)(store.getState());
-console.log(bugByUserId);
+// store.dispatch(userAdded({ name: "Mosh" }));
+// store.dispatch(projectAdded({ name: "project 1" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugAdded({ description: "Bug 4" }));
+// store.dispatch(bugAdded({ description: "Bug 5" }));
+// store.dispatch(bugAdded({ description: "Bug 6" }));
+// const bugByUserId = getBugsByUser(1)(store.getState());
+// console.log(bugByUserId);
 
-//this is a higher level function which takes bug 1 as their argument
-
-store.dispatch(bugResolved({ id: 1 }));
-unsubscribe();
-store.dispatch(bugRemoved({ id: 2 }));
+// store.dispatch(bugResolved({ id: 1 }));
+// unsubscribe();
+// store.dispatch(bugRemoved({ id: 2 }));
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
+import logger from "./middleware/logger";
 
 //this is a higher orde function because it take a function as an argument
 
 export default function () {
-  return configureStore({ reducer: reducer });
+  return configureStore({
+    reducer: reducer,
+    middleware: [logger],
+  });
 }
+
+//since we are using redux toolkit here we can pass middleware like this
+// if we used createStore from redux then the process would have been different
+
+//would pass it like this
+
+// const store = createStore(reducer, applyMiddleware(logger))
